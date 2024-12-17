b7d727d0a78f2d4001c254fd2ae5f8d695631f8d
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-12-17 21:12:36 +0300

    fix: update font

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/shared/ui/sonner'
 
-const poiret = Inter({
+const inter = Inter({
   variable: '--inter',
   subsets: ['latin'],
-  weight: ['400'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ru'>
-      <body className={`${poiret.className} antialiased`}>
+      <body className={`${inter.className} antialiased`}>
         {children}
         <Toaster />
       </body>
