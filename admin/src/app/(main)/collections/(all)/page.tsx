4145bdbcda1835238be362d89b5ed598335134b0
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-12-26 21:21:37 +0300

    refactor: update pages

--- admin/src/app/(main)/collections/(all)/page.tsx
+++ admin/src/app/(main)/collections/(all)/page.tsx
@@ -6,12 +6,22 @@ export default async function CollectionsPage() {
   return (
     <main className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 px-6 py-2'>
       {!success && (
-        <h1 className='text-3xl font-semibold text-red-500'>Ошибка загрузки коллекций.</h1>
+        <h1 className='text-3xl font-semibold text-red-500 col-span-3'>
+          Ошибка загрузки коллекций.
+        </h1>
       )}
-      {collections &&
-        collections.map((collection) => (
-          <CollectionCard key={collection.id} collection={collection} />
-        ))}
+      {collections && (
+        <>
+          {collections.length === 0 && (
+            <h1 className='text-2xl font-semibold col-span-3'>
+              Вы пока не создали ни одну коллекцию.
+            </h1>
+          )}
+          {collections.map((collection) => (
+            <CollectionCard key={collection.id} collection={collection} />
+          ))}
+        </>
+      )}
     </main>
   )
 }
--- admin/src/app/(main)/collections/[id]/page.tsx
+++ admin/src/app/(main)/collections/[id]/page.tsx
@@ -8,8 +8,6 @@ export default async function CollectionPage({ params }: { params: Promise<{ id:
   const [{ data: collection, success: collectionSuccess }, { data: items, success: itemsSuccess }] =
     await Promise.all([getCollection(id), getCollectionItemsByCollection(id)])
 
-  const hasItems = items && items.length > 0
-
   return (
     <main className='flex flex-col gap-4 px-6 py-2'>
       <section>
@@ -20,18 +18,23 @@ export default async function CollectionPage({ params }: { params: Promise<{ id:
         )}
       </section>
 
-      <h2 className='font-bold text-2xl'>Украшения:</h2>
-
-      {!itemsSuccess ? (
+      {!itemsSuccess && (
         <h1 className='text-2xl font-semibold text-red-500'>Ошибка загрузки украшений.</h1>
-      ) : (
-        <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
-          {!hasItems ? (
-            <h2 className='font-semibold text-xl'>Украшений в коллекции нет</h2>
-          ) : (
-            items.map((item) => <CollectionItemCard key={item.id} item={item} />)
-          )}
-        </section>
+      )}
+
+      {items && (
+        <>
+          <h2 className='font-bold text-2xl'>
+            {items.length > 0
+              ? 'Украшения в коллекции:'
+              : 'Вы пока не добавили ни одного украшения в коллекцию.'}
+          </h2>
+          <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            {items.map((item) => (
+              <CollectionItemCard key={item.id} item={item} />
+            ))}
+          </section>
+        </>
       )}
     </main>
   )
