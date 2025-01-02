2313b9c076f9350babd5ee790ee275f02cc09707
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-01-02 22:43:41 +0300

    feat: add modal image

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,17 @@
-import Image from 'next/image'
+import { Image } from '@/shared/ui/image'
 import contents from '../assets/main.json'
-import mikhail from '../assets/mikhail.jpg'
 import { Contacts } from '@/features/contacts'
 import { Button } from '@/shared/ui/button'
 
 export default function Home() {
   return (
     <main>
-      <Image src={mikhail} alt='Mikhail' />
+      <Image
+        src={'collections/e061afa3-2286-4c04-8419-57beb169c48a'}
+        alt='Mikhail'
+        width={500}
+        height={500}
+      />
       <Contacts>
         <Button>Контакты</Button>
       </Contacts>
--- web/src/shared/ui/image.tsx
+++ web/src/shared/ui/image.tsx
@@ -1,19 +1,55 @@
 'use client'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/shared/ui/dialog'
 import { default as NextImage, ImageProps } from 'next/image'
 import { useState } from 'react'
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import { IMAGE_URL } from '../lib/constants'
+import { cn } from '../lib/utils'
 
-export function Image({ src, alt, ...props }: ImageProps) {
+interface Props extends ImageProps {
+  className?: string
+  previewClassName?: string
+}
+export function Image({ src, alt, className, previewClassName, ...props }: Props) {
   const [isLoaded, setLoaded] = useState(false)
 
   return (
-    <NextImage
-      {...props}
-      alt={alt}
-      src={`${IMAGE_URL}/${src}${isLoaded ? '_high.jpg' : '_low.jpg'}`}
-      blurDataURL='/placeholder.jpg'
-      placeholder='blur'
-      onLoad={() => setLoaded(true)}
-    />
+    <Dialog>
+      <DialogTrigger asChild>
+        <NextImage
+          {...props}
+          alt={alt}
+          src={`${IMAGE_URL}/${src}${isLoaded ? '_high.jpg' : '_low.jpg'}`}
+          blurDataURL='/placeholder.jpg'
+          placeholder='blur'
+          onLoad={() => setLoaded(true)}
+          className={cn('rounded-lg cursor-pointer', className)}
+        />
+      </DialogTrigger>
+      <DialogContent className='p-0 m-0 border-none bg-transparent flex items-center justify-center shadow-none'>
+        <VisuallyHidden>
+          <DialogTitle>{alt}</DialogTitle>
+          <DialogDescription>{alt}</DialogDescription>
+        </VisuallyHidden>
+        <DialogHeader className='max-h-[90vh] overflow-auto flex justify-center'>
+          <NextImage
+            {...props}
+            alt={alt}
+            src={`${IMAGE_URL}/${src}${isLoaded ? '_high.jpg' : '_low.jpg'}`}
+            blurDataURL='/placeholder.jpg'
+            placeholder='blur'
+            onLoad={() => setLoaded(true)}
+            className={cn('object-cover relative rounded-lg max-h-[90vh]', previewClassName)}
+          />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
